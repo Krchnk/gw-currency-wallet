bfd5b1cb1f878836ec422bdf75e22ee306122514
Krchnk <a4mesh@gmail.com>
2025-03-18 02:25:45 +0300

    //

--- cmd/main.go
+++ cmd/main.go
@@ -75,9 +75,11 @@ func main() {
 
 func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://currency-wallet-frontend.onrender.com")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://front-wallet.onrender.com")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Обрабатываем предварительный запрос OPTIONS
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
